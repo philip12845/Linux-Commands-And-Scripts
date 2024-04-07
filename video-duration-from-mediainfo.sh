7fca019a9fa3b7c5e240ce4810c31b2b722f04b4
philip12845 <87152429+philip12845@users.noreply.github.com>
2024-04-07 15:37:55 +0100

    Add files via upload

---
+++ video-duration-from-mediainfo.sh
@@ -0,0 +1,1 @@
+mediainfo --Output="Video;%Duration%\n" ./ | awk '{ sum += $1 } END { secs=sum/1000; h=int(secs/3600);m=int((secs-h*3600)/60);s=int(secs-h*3600-m*60); printf("%02d:%02d:%02d\n",h,m,s) }'
