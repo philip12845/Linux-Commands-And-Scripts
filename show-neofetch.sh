56ea62f152587deaf98df99c3d44ea510abde807
philip12845 <87152429+philip12845@users.noreply.github.com>
2024-06-15 21:40:16 +0100

    Update show-neofetch.sh
    added "&& source ~/.bashrc" to all of these as this will update it as soon as changes are made

--- show-neofetch.sh
+++ show-neofetch.sh
@@ -1,8 +1,8 @@
 # Show neofetch upon login or when new terminal window has opened
-echo neofetch >> .bashrc
+echo neofetch >> .bashrc && source ~/.bashrc
 
 # Re-run neofetch when you use the clear command to clear the screen
-echo 'alias clear=clear && neofetch' >> .bashrc
+echo 'alias clear=clear && neofetch' >> .bashrc && source ~/.bashrc
 
 # Do both of the above
-echo 'alias clear=clear && neofetch' >> .bashrc && echo neofetch >> .bashrc
+echo 'alias clear=clear && neofetch' >> .bashrc && echo neofetch >> .bashrc && source ~/.bashrc
